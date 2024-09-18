a345c8d5f1a730ffdf88cf1ee07061b9d47ef708
Segment Git Sync <friends@segment.com>
2024-09-18 20:35:11 +0000

    Full sync

---
+++ user_groups/2N3Y356kJMhmdWUd2pDsfzCiQ6j.tf
@@ -0,0 +1,88 @@
+import {
+  to = segment_user_group.id-2N3Y356kJMhmdWUd2pDsfzCiQ6j
+  id = "2N3Y356kJMhmdWUd2pDsfzCiQ6j"
+}
+
+resource "segment_user_group" "id-2N3Y356kJMhmdWUd2pDsfzCiQ6j" {
+  members = ["adrian.bogle@drip.com", "anshuman.dikhit@drip.com", "hbc@sleeknote.com", "lars@larsfriis.io", "michael.nadel@drip.com", "sebastian.vaeggemose@drip.com", "tianna.avery@drip.com", "william.johnston@drip.com"]
+  name    = "Contribute"
+  permissions = [
+    {
+      resources = [
+        {
+          id = "mbj3HAIe6A"
+          labels = [
+          ]
+          type = "WORKSPACE"
+        },
+      ]
+      role_id = "1Mc8DEHhMqqSkb5cPjtu9pAEyNf"
+    },
+    {
+      resources = [
+        {
+          id = "mbj3HAIe6A"
+          labels = [
+          ]
+          type = "WORKSPACE"
+        },
+      ]
+      role_id = "1Mc8DFJ5XdtkAaokYZCgoiUr6LJ"
+    },
+    {
+      resources = [
+        {
+          id = "mbj3HAIe6A"
+          labels = [
+          ]
+          type = "WORKSPACE"
+        },
+      ]
+      role_id = "1Mc8DHU42rYyIp94oBPexlvYpf8"
+    },
+    {
+      resources = [
+        {
+          id = "mbj3HAIe6A"
+          labels = [
+          ]
+          type = "WORKSPACE"
+        },
+      ]
+      role_id = "1Mc8DKqCj3vrDvNml9Vhsc7Aebt"
+    },
+    {
+      resources = [
+        {
+          id = "mbj3HAIe6A"
+          labels = [
+          ]
+          type = "WORKSPACE"
+        },
+      ]
+      role_id = "1PWCliYHNGD6q2xVM3MjSCD5WRU"
+    },
+    {
+      resources = [
+        {
+          id = "mbj3HAIe6A"
+          labels = [
+          ]
+          type = "WORKSPACE"
+        },
+      ]
+      role_id = "1Ttjwdg1SEGKH5hfg8heUBlfXGK"
+    },
+    {
+      resources = [
+        {
+          id = "mbj3HAIe6A"
+          labels = [
+          ]
+          type = "WORKSPACE"
+        },
+      ]
+      role_id = "1ZxNOhjYFrqvYCPreLHBNTnepB4"
+    },
+  ]
+}
