498b2820a438449c6544eb95c7c6146a904ad7dd
Segment Git Sync <friends@segment.com>
2024-09-18 20:35:05 +0000

    Full sync

---
+++ warehouses/nDKFTsPmaQHBU8397p67gR.tf
@@ -0,0 +1,13 @@
+import {
+  to = segment_warehouse.id-nDKFTsPmaQHBU8397p67gR
+  id = "nDKFTsPmaQHBU8397p67gR"
+}
+
+resource "segment_warehouse" "id-nDKFTsPmaQHBU8397p67gR" {
+  enabled = true
+  metadata = {
+    id = "aea3c55dsz"
+  }
+  name     = "\"Redshift DataMart\""
+  settings = "{\"ciphertext\":\"AQICAHhDxiwP/vgvvqzyHLPVxM5a7lEjWTT0CiiQB+LPknr0YAFqUb6oBkU4acaGfzH3KhmPAAAAdzB1BgkqhkiG9w0BBwagaDBmAgEAMGEGCSqGSIb3DQEHATAeBglghkgBZQMEAS4wEQQMIRGeujdltKDEA+qgAgEQgDTAGufYJzEvxERbGcMXdro5qVpBi2NFDedByqqcM1VWvKF83OBYtVUQWd1upoSsTOALffc7\",\"database\":\"dumps\",\"encrypted\":true,\"host\":\"drip-data-mart-1.ciweqdnqtr0x.us-east-1.redshift.amazonaws.com\",\"hostname\":\"drip-data-mart-1.ciweqdnqtr0x.us-east-1.redshift.amazonaws.com\",\"port\":\"5439\",\"username\":\"segment\"}"
+}
