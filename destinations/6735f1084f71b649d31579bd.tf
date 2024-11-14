3fd6338cd0023644b5165c518602fba239e89565
Segment Git Sync <friends@segment.com>
2024-11-14 12:46:01 +0000

    Destination '6735f1084f71b649d31579bd' created by patrick.moog@drip.com

---
+++ destinations/6735f1084f71b649d31579bd.tf
@@ -0,0 +1,21 @@
+import {
+  to = segment_destination.id-6735f1084f71b649d31579bd
+  id = "6735f1084f71b649d31579bd"
+}
+
+resource "segment_destination" "id-6735f1084f71b649d31579bd" {
+  enabled = false
+  metadata = {
+    contacts = [
+      {
+      },
+    ]
+    id                = "5ece242d61055a0b1bb2e103"
+    partner_owned     = true
+    region_endpoints  = null
+    supported_regions = ["us-west-2"]
+  }
+  name      = "PostHog Sleeknote"
+  settings  = "{\"apiKey\":\"\",\"postHogInstance\":\"\"}"
+  source_id = "7SfAMqPGrz"
+}
