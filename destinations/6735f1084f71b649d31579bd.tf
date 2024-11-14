0d1383ec61f68fe48ee0c443e7762d29276cc39a
Segment Git Sync <friends@segment.com>
2024-11-14 12:47:02 +0000

    Destination '6735f1084f71b649d31579bd' modified by patrick.moog@drip.com

--- destinations/6735f1084f71b649d31579bd.tf
+++ destinations/6735f1084f71b649d31579bd.tf
@@ -16,6 +16,6 @@ resource "segment_destination" "id-6735f1084f71b649d31579bd" {
     supported_regions = ["us-west-2"]
   }
   name      = "PostHog Sleeknote"
-  settings  = "{\"apiKey\":\"••••••••••hsmP\",\"postHogInstance\":\"\"}"
+  settings  = "{\"apiKey\":\"••••••••••hsmP\",\"postHogInstance\":\"eu.posthog.com\"}"
   source_id = "7SfAMqPGrz"
 }
